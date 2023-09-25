4e7df9fc5dc8c54be3a426a2355a1ece3fbee64e
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-09-25 10:43:19 +0600

    added t-shirt project

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+
+
+
+const About = () => {
+    return (
+        <div>
+            <h3>This is ABout page bro</h3>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+
+const Navbar = () => {
+    return (
+        <div className="flex justify-between bg-black p-4 ">
+            <div className="text-white hover:text-orange-600">
+                <Link to="/" >T-Shirt</Link>
+            </div>
+            <div className="grid  grid-cols-4 gap-20">
+                <Link className="text-white hover:text-orange-600" to="/" >Home</Link>
+                <Link className="text-white hover:text-orange-600" to="/about" >About</Link>
+                <Link className="text-white hover:text-orange-600" to="/blog" >Blog</Link>
+                <Link className="text-white hover:text-orange-600" to="/login" >Login</Link>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
